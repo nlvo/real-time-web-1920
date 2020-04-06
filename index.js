013a45686aa797bb63343048faff01d26269d853
Lien Vo <nlvo@live.nl>
2020-04-06 13:03:38 +0200

    add basic chat app with vanilla js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = 3000;
+
+app  
+    .use(express.static('public'))
+    .get('/', function(req, res){
+        res.sendFile(__dirname + '/index.html')
+    });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat', msg)
+    });
+});
+
+http.listen(port, function(){
+    console.log(`listening on*:${port}`);
+})
